ef04aec5ab71d85f17275072c0ecffccf6bc7e04
Nidyra <ellisondearaujodare@gmail.com>
2025-08-05 08:58:36 -0300

    Resolve Error: appTheme not Function

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -3,8 +3,8 @@ import type { DefaultTheme } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     body, html {
-        background:${(props) => props.theme.appBackground};
-        color: ${(props) => props.theme.appColor};
+        background:${(props) => props.theme};
+        color: ${(props) => props.theme};
         margin: 0;
         padding: 0;
         font-family: "Inter", sans-serif;

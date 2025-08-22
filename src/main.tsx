fed378e5d2e0dabdd150e5948505084d9dcb0f2f
Nidyra <ellisondearaujodare@gmail.com>
2025-08-22 09:19:06 -0300

    Resolve error

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, lighTheme, darkTheme } from './styles/'
+import { GlobalStyle, lighTheme } from './styles/'
 
 createRoot(document.getElementById('root')!).render(
   <React.Fragment>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lighTheme}>
       <GlobalStyle/>
       <App />
     </ThemeProvider>
